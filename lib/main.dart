f402fdde2fcbdb941f7a15184b97529257947f14
dipesh p <dipesh.p@tejimandi.com>
2022-03-26 16:06:57 +0530

    Firebase Initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:money_tracker/firebase_options.dart';
 import 'package:money_tracker/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(Main());
 }
 
