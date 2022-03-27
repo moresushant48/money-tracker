ea38f582a6e06f4fc95278040ca051435d651175
Sushant More <moresushant48@gmail.com>
2022-03-27 17:01:59 +0530

    Set Domain Url & use UID for url path.

--- lib/screens/homepage/repo/domain_repo.dart
+++ lib/screens/homepage/repo/domain_repo.dart
@@ -1,15 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:money_tracker/models/domain.dart';
 
 class DomainRepo {
   late FirebaseDatabase db;
+  late FirebaseAuth auth;
 
   DomainRepo() {
+    auth = FirebaseAuth.instance;
     db = FirebaseDatabase.instance;
   }
 
+  getDomainsUrl() {
+    return "users/${auth.currentUser!.uid}/domains/";
+  }
+
   Future<List<DomainModel>> fetchDomains() async {
-    DataSnapshot snap = await db.ref("users/sushant/").get();
+    DataSnapshot snap = await db.ref(getDomainsUrl()).get();
     List<DomainModel> domains = [];
 
     if (snap.value != null && snap.value != "") {
@@ -23,10 +30,10 @@ class DomainRepo {
   }
 
   Future<void> addDomain(String domain) {
-    return db.ref("users/sushant/").child(domain).set("");
+    return db.ref(getDomainsUrl()).child(domain).set("");
   }
 
   removeDomain(String domain) {
-    db.ref("users/sushant/").child(domain).remove();
+    db.ref(getDomainsUrl()).child(domain).remove();
   }
 }
